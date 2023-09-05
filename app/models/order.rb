5cc65cb5a1e8623a75bc5356af2d9c09d4ed6bcf
markcgarjas <markcarlo859@gmail.com>
2023-09-05 21:46:34 +0800

    Add Capturing an Order

---
+++ app/models/order.rb
@@ -0,0 +1,15 @@
+class Order < ApplicationRecord
+  enum pay_type: { client: 0, admin: 1, super_admin: 2 }
+
+  has_many :line_items, dependent: :destroy
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
+end
